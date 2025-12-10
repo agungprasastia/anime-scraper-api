567fb2e4ef59e9de67cca0710bb6d517da3ff6e6
Agung Prasasti Abadi <agungprasastia@gmail.com>
2025-12-10 08:56:55 +0800

    refactor: replace Express with Elysia for improved performance and simplicity

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import express from "express";
+import { Elysia } from "elysia";
 import dotenv from "dotenv";
 import { router } from "./src/api.js";
 import "./src/worker.js";
 
 dotenv.config();
 
-const app = express();
-app.use(express.json());
-app.use(router);
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("API running on port", PORT));
+
+const app = new Elysia()
+    .use(router)
+    .listen(PORT);
+
+console.log(`API running on port ${app.server?.port}`);
